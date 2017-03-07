a1ce2617c75daf40bde1e3a04bd18483e70f5114
elizabeth boles <elizabethboles@elizabeths-MacBook-Air.local>
2017-03-06 22:30:33 -0500

    got the stars in their counts to log out

--- main.js
+++ main.js
@@ -1,34 +1,59 @@
-let fetch = require('node-fetch');
-console.log( process.argv[2] );
 
+
+let fetch = require('node-fetch');
+let userName = process.argv[2];
 let promise = fetch(
-    'https://api.github.com/users/jakerella',
+    'https://api.github.com/users/' + userName,
     {
         method: 'GET',
         headers: {
-            Authorization: 'token ' + process.argv[2]
-        },
-        body: '...'  // POST (creating data) or PATCH (updating)
+            Authorization: 'token ' + process.argv[3]
+        }
     }
 );
 
 promise.then( function handleResponse(responseObj) {
-    console.log( responseObj.status );
+    //console.log( responseObj.status );
 
     if (responseObj.status > 199 && responseObj.status < 300) {
-        // In here, I know the request was successful
 
-        responseObj.json().then( function printData(myUserData) {
+        responseObj.json().then(function printData(myUserData) {
             console.log( myUserData.name, myUserData.location );
             //put the . to target the name and location after the iterm print out
-
-
-        } );
+        });
 
     } else {
-        // Now I know there was a problem
-        // Maybe we should tell the user!!
         console.log( 'There was a problem', responseObj.status );
     }
+     });
+//-----------------------------------------------------------------------------
+let promise2 = fetch(
+    'https://api.github.com/users/' + userName + '/repos',
+    {
+        method: 'GET',
+        headers: {
+            Authorization: 'token ' + process.argv[3]
+        }
+    }
+);
 
+promise2.then( function repoResponse(response) {
+    //console.log(response.status);
+
+    if(response.status > 199 && response.status < 300) {
+      response.json().then(function printData(reposData) {
+
+
+        let totalStarCount;
+        let totalStarCountNumber = 0;
+
+        reposData.forEach( function retrievesCount(repository) {
+          console.log(repository.stargazers_count, repository.name);
+        });
+
+      });
+    } else {
+      console.log( 'There was a problem', response.status );
+    }
 });
+//-----------------------------------------------------------------------------
